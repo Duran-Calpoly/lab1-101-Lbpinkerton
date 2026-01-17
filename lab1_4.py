88ae679a403da87c1e73306cdf87936db8a64a26
Lbpinkerton <lbpinkerton07@yahoo.com>
2026-01-17 02:22:09 +0000

    Lab 1 Done

--- lab1_4.py
+++ lab1_4.py
@@ -1,1 +1,13 @@
-pass
+def calculate_average(num1, num2, num3):
+    """Calculate the mathematical average of three numbers."""
+    return ((num1 + num2 + num3) / 3)
+
+
+def add_tax(bill_total):
+    """Return the total after adding a 10% sales tax."""
+    return (bill_total * 1.10)
+
+
+def greet_user(name):
+    """Return a greeting that says 'Hello' followed by the user's name."""
+    return (f'Hello {name}')
--- lab1_5.py
+++ lab1_5.py
@@ -1,1 +1,22 @@
-pass
+def check_multiple(number):
+    """Check if the number is a multiple of both 3 and 5."""
+    return number % 3 == 0 and number % 5 == 0
+def check_password(input_string):
+    secret_word = "Python123"
+    if input_string == secret_word:
+        return "access granted"
+    else:
+        return "access denied"
+def calculate_federal_tax(salary):
+    """Calculate federal tax based on salary brackets."""
+    if salary <= 11000:
+        tax_rate = 0.10
+    elif salary <= 44725:
+        tax_rate = 0.12
+    elif salary <= 95375:
+        tax_rate = 0.22
+    elif salary <= 182100:
+        tax_rate = 0.24
+    else:
+        tax_rate = 0.32
+    return salary * tax_rate
